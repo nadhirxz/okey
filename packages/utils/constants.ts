f38319e56ab9df880540f8085e8ef243e72a7c7e
Nadhir Ferdji <nadhirxz@gmail.com>
2024-03-23 02:10:33 +0100

    Add @okey/utils

---
+++ packages/utils/constants.ts
@@ -0,0 +1,6 @@
+const TILE_COLORS = ['blue', 'red', 'yellow', 'black'] as const;
+const TILE_NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] as const;
+const NUMBER_OF_PLAYERS = 4;
+const NUMBER_OF_TILES_PER_PLAYER = 21;
+
+export { TILE_COLORS, TILE_NUMBERS, NUMBER_OF_PLAYERS, NUMBER_OF_TILES_PER_PLAYER };
