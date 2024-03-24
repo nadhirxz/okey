710a0d199058cf625ac5caedc27da4541f7024c5
Nadhir Ferdji <nadhirxz@gmail.com>
2024-03-25 00:02:01 +0100

    server: add helmet

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(helmet())
 
 app.use(express.static(path.join(__dirname, '../../web')));
 
