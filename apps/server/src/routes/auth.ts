1ed37aee930214fcaf4534752a4b31c15df509a9
Nadhir Ferdji <nadhirxz@gmail.com>
2024-03-27 01:52:30 +0100

    server: add login route

---
+++ apps/server/src/routes/auth.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import prisma from '@okey/database/client';
+import { verifyPassword } from '@okey/utils/hashing';
+import { generateAccessToken, generateRefreshToken } from '@okey/auth';
+import { noAuth } from '../middlewares/auth';
+
+const router = Router();
+
+router.post('/login', noAuth, async (req, res) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) return res.status(400).json({ success: false, error: 'Please provide email and password' });
+
+	const user = await prisma.user.findUnique({ where: { email } });
+
+	if (!user) return res.status(404).json({ success: false, error: 'User not found' });
+
+	const passwordVerification = await verifyPassword(user.password, password);
+
+	if (!passwordVerification) return res.status(401).json({ success: false, error: 'Invalid password' });
+
+	const accessToken = generateAccessToken(user);
+	const refreshToken = generateRefreshToken(user);
+
+	res.cookie('access_token', accessToken)
+		.status(200)
+		.json({
+			success: true,
+			refreshToken,
+			user: {
+				name: user.name,
+			},
+		});
+});
+
+export default router;
--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import authRoutes from './auth';
 
 const router = Router();
 
 router.get('/status', (_, res) => res.send('ok'));
+
+router.use('/auth', authRoutes);
 
 export default router;
