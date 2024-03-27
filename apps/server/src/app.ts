19228da5bcadc02d613f9974b6d6de60dac6b5a8
Nadhir Ferdji <nadhirxz@gmail.com>
2024-03-27 01:49:01 +0100

    server: add cookie parser

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(helmet())
+app.use(helmet());
+app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '../../web')));
 
