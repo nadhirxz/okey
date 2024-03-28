b693d1dd103f5050e0d272b4e72e751fe7957f99
Nadhir Ferdji <nadhirxz@gmail.com>
2024-03-28 02:46:03 +0100

    server: socket connection handling & game start logic

--- apps/server/src/io/index.ts
+++ apps/server/src/io/index.ts
@@ -1,17 +1,36 @@
 import type { Server } from 'socket.io';
+import Game from '../game/game';
+import { getTokenPayload } from '@okey/auth';
+import userStatusManager from '../services/status';
+import { NUMBER_OF_PLAYERS } from '@okey/utils/constants';
 
-let onlinePlayers = new Array(); //TODO: Should not exceed 4 players!
-var currentPlayer = 1;
-let drawTile = false; // !???
-let remainingDeck = [];
+const game = new Game();
 
 export const initIO = (io: Server) => {
 	io.on('connection', socket => {
-		console.log('User connected: ' + socket.id);
-		io.emit('players', onlinePlayers);
-	});
+		const { token } = socket.handshake.auth;
 
-	io.on('disconnect', reason => {
-		console.log('a user disconnected:', reason);
+		const user = getTokenPayload(token);
+		if (!user) {
+			socket.disconnect();
+			return;
+		}
+
+		console.log('a user connected:', user);
+		userStatusManager.goOnline(user.id);
+		socket.join(game.id);
+
+		socket.on('disconnect', () => {
+			game.removePlayer(user.id);
+			userStatusManager.goOffline(user.id);
+
+			console.log('user disconnected:', user);
+		});
+
+		const playerIDs = game.addPlayer(user.id);
+		if (playerIDs.length == NUMBER_OF_PLAYERS) {
+			game.start();
+			io.to(game.id).emit('game-start', game.players);
+		}
 	});
 };
