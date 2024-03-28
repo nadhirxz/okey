145d1069457cc99d450ffb413c9684faf195462b
Nadhir Ferdji <nadhirxz@gmail.com>
2024-03-28 02:44:02 +0100

    server: add cors

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -3,12 +3,17 @@ import path from 'path';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import router from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+	origin: ['http://127.0.0.1:3000'],
+	credentials: true,
+}))
 
 app.use(express.static(path.join(__dirname, '../../web')));
 
