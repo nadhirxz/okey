9d6fe617224f9fcbdda217fd0624c4b02bea8547
Nadhir Ferdji <nadhirxz@gmail.com>
2024-03-28 02:46:13 +0100

    server: remove unnecessary types

--- apps/server/src/types/game.ts
+++ apps/server/src/types/game.ts
@@ -1,11 +1,7 @@
-import { NUMBER_OF_PLAYERS, SPECIAL_TILES, TILE_COLORS, TILE_NUMBERS } from '@okey/utils/constants';
+import { SPECIAL_TILES, TILE_COLORS, TILE_NUMBERS } from '@okey/utils/constants';
 
 type TileColor = (typeof TILE_COLORS)[number] | (typeof SPECIAL_TILES)[number];
 type TileNumber = (typeof TILE_NUMBERS)[number];
 type TileString = `${TileNumber}-${TileColor}`;
 
-type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N ? Acc[number] : Enumerate<N, [...Acc, Acc['length']]>;
-type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
-type PlayerId = IntRange<0, typeof NUMBER_OF_PLAYERS>;
-
-export { TileColor, TileNumber, TileString, PlayerId };
+export { TileColor, TileNumber, TileString };
